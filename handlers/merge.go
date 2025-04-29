09b84c7046caf18455f2d92dfce9f927bd0cb7da
Gasoid <gasoid@gmail.com>
2025-04-29 11:51:49 +0200

    update is workind

--- handlers/merge.go
+++ handlers/merge.go
@@ -4,13 +4,14 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"errors"
+	"log/slog"
 	"net/url"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/ldez/go-git-cmd-wrapper/v2/checkout"
 	"github.com/ldez/go-git-cmd-wrapper/v2/clone"
-	"github.com/ldez/go-git-cmd-wrapper/v2/config"
 	"github.com/ldez/go-git-cmd-wrapper/v2/git"
 	"github.com/ldez/go-git-cmd-wrapper/v2/merge"
 	"github.com/ldez/go-git-cmd-wrapper/v2/push"
@@ -34,29 +35,40 @@ func MergeMaster(username, password, repoUrl, branchName, master string) error {
 	hasher.Write([]byte(repoUrl))
 	dir := hex.EncodeToString(hasher.Sum([]byte(strings.Join([]string{repoUrl, branchName}, ","))))
 
+	cur, err := os.Getwd()
+	if err != nil {
+		slog.Debug("Getwd error")
+		return err
+	}
+
+	defer os.RemoveAll(path.Join(cur, dir))
+
 	if _, err := os.Stat(dir); !os.IsNotExist(err) {
 		return errors.New("directory exists")
 	}
 
-	defer os.RemoveAll(dir)
-
 	if _, err := git.Clone(clone.Repository(repoUrl), clone.Directory(dir)); err != nil {
+		slog.Debug("git clone error")
+		return err
+	}
+
+	if err := os.Chdir(dir); err != nil {
+		slog.Debug("chdir error")
 		return err
 	}
 
 	if _, err := git.Checkout(checkout.Branch(branchName)); err != nil {
-		return err
-	}
-
-	if _, err := git.Config(config.Entry("user.email", "bot@example.com"), config.Entry("user.name", "merge-bot")); err != nil {
+		slog.Debug("git checkout error", "branch", branchName)
 		return err
 	}
 
 	if _, err := git.Merge(merge.NoFf, merge.Commits(master)); err != nil {
+		slog.Debug("git merge error")
 		return err
 	}
 
 	if _, err := git.Push(push.Remote(defaultRemote), push.RefSpec(branchName)); err != nil {
+		slog.Debug("git push error")
 		return err
 	}
 
