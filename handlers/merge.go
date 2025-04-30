c68fa275583e37608356d2f01bf21801183e3e11
Gasoid <gasoid@gmail.com>
2025-04-30 09:04:56 +0200

    remove all

--- handlers/merge.go
+++ handlers/merge.go
@@ -32,7 +32,7 @@ func MergeMaster(username, password, repoUrl, branchName, master string) error {
 		return err
 	}
 
-	defer os.RemoveAll(dir)
+	// defer os.RemoveAll(dir)
 
 	if _, err := git.Clone(clone.Repository(repoUrl), clone.Directory(dir)); err != nil {
 		slog.Debug("git clone error", "dir", dir)
