89888a66c2fa24a727417e826ccc7237259846da
Gasoid <gasoid@gmail.com>
2025-07-12 16:23:46 +0200

    fix: remove obsolete comments

--- handlers/merge.go
+++ handlers/merge.go
@@ -20,14 +20,8 @@ const (
 	defaultRemote = "origin"
 )
 
-// var (
-// 	mergeLock sync.Mutex
-// )
-
 //nolint:errcheck
 func MergeMaster(username, password, repoUrl, branchName, master string) error {
-	// mergeLock.Lock()
-	// defer mergeLock.Unlock()
 
 	if username != "" && password != "" {
 		parsedUrl, err := url.Parse(repoUrl)
@@ -49,24 +43,12 @@ func MergeMaster(username, password, repoUrl, branchName, master string) error {
 		g.AddBaseOptions(dir)
 	}
 
-	// currentDir, err := os.Getwd()
-	// if err != nil {
-	// 	logger.Debug("getwd error", "error", err)
-	// 	currentDir = "/tmp/"
-	// }
-
 	defer os.RemoveAll(dir)
-	// defer os.Chdir(currentDir)
 
 	if output, err := git.Clone(clone.Repository(repoUrl), clone.Directory(dir)); err != nil {
 		logger.Debug("git clone error", "dir", dir, "output", output)
 		return fmt.Errorf("git clone error: %w, output: %s", err, output)
 	}
-
-	// if err := os.Chdir(dir); err != nil {
-	// 	logger.Debug("chdir error")
-	// 	return fmt.Errorf("chdir error: %w", err)
-	// }
 
 	if output, err := git.Config(workingDir, config.Entry("user.email", fmt.Sprintf("%s@localhost", username))); err != nil {
 		logger.Debug("git config error", "user.email", fmt.Sprintf("%s@localhost", username), "output", output)
