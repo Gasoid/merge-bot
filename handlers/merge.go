4656532de8e9e94e370224e0e4c9c7599910c37e
SSgeek <ssgeek@hotmail.com>
2025-12-10 16:44:54 +0800

    fix(merge): merge cmd bug

--- handlers/merge.go
+++ handlers/merge.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ldez/go-git-cmd-wrapper/v2/checkout"
 	"github.com/ldez/go-git-cmd-wrapper/v2/clone"
 	"github.com/ldez/go-git-cmd-wrapper/v2/config"
+	"github.com/ldez/go-git-cmd-wrapper/v2/fetch"
 	"github.com/ldez/go-git-cmd-wrapper/v2/git"
 	"github.com/ldez/go-git-cmd-wrapper/v2/global"
 	"github.com/ldez/go-git-cmd-wrapper/v2/merge"
@@ -66,9 +67,9 @@ func MergeMaster(username, password, repoUrl, branchName, master string) error {
 		return fmt.Errorf("git config error: %w, output: %s", err, output)
 	}
 
-	if output, err := git.Checkout(workingDir, checkout.Branch(master)); err != nil {
-		logger.Debug("git checkout error", "branch", master, "output", output)
-		return fmt.Errorf("git checkout error: %w, output: %s", err, output)
+	if output, err := git.Fetch(workingDir, fetch.All); err != nil {
+		logger.Debug("git fetch error", "branch", branchName, "output", output)
+		return fmt.Errorf("git fetch error: %w, output: %s", err, output)
 	}
 
 	if output, err := git.Checkout(workingDir, checkout.Branch(branchName)); err != nil {
@@ -76,9 +77,10 @@ func MergeMaster(username, password, repoUrl, branchName, master string) error {
 		return fmt.Errorf("git checkout error: %w, output: %s", err, output)
 	}
 
-	if output, err := git.Merge(workingDir, merge.Commits(master), merge.M(fmt.Sprintf("✨ merged %s", master))); err != nil {
+	remoteTargetBranch := fmt.Sprintf("%s/%s", defaultRemote, master)
+	if output, err := git.Merge(workingDir, merge.Commits(remoteTargetBranch), merge.M(fmt.Sprintf("✨ merged %s", master))); err != nil {
 		logger.Debug("git merge error", "output", output)
-		if output, err := git.Merge(workingDir, merge.NoFf, merge.Commits(master), merge.M(fmt.Sprintf("✨ merged %s", master))); err != nil {
+		if output, err := git.Merge(workingDir, merge.NoFf, merge.Commits(remoteTargetBranch), merge.M(fmt.Sprintf("✨ merged %s", master))); err != nil {
 			logger.Debug("git merge --no-ff error", "output", output)
 
 			mergeError := &MergeError{
