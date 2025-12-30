7c30b01cd28d27ae8a43f9b977123a9255cf7a4d
Gasoid <gasoid@gmail.com>
2025-12-30 19:45:49 +0100

    fix: structure

---
+++ plugins/plugins.go
@@ -0,0 +1,143 @@
+package plugins
+
+import (
+	"io"
+	"iter"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/gasoid/merge-bot/config"
+	"github.com/gasoid/merge-bot/handlers"
+	"github.com/gasoid/merge-bot/logger"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	githubURL = "https://github.com/"
+	gitlabURL = "https://gitlab.com/"
+)
+
+var (
+	plugins   string
+	engines   map[string]func([]byte) (HandlerFunc, error)
+	enginesMu sync.RWMutex
+)
+
+type HandlerFunc func(command *handlers.Request, args string) error
+
+func init() {
+	config.StringVar(&plugins, "plugins", "", "comma list of plugin urls (also via PLUGINS)")
+}
+
+type PluginManifest struct {
+	Name    string      `yaml:"name"`
+	Command string      `yaml:"command"`
+	Runtime string      `yaml:"runtime"`
+	Handler HandlerFunc `yaml:"-"`
+}
+
+func Register(name string, constructor func([]byte) (HandlerFunc, error)) {
+	enginesMu.Lock()
+	defer enginesMu.Unlock()
+	engines[name] = constructor
+}
+
+//nolint:errcheck
+func downloadFile(fileUrl string) ([]byte, error) {
+	resp, err := http.Get(fileUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func getFile(filePath string) ([]byte, error) {
+	if strings.HasPrefix(filePath, "https://") {
+		return downloadFile(filePath)
+	}
+
+	return os.ReadFile(filePath)
+}
+
+func getManifest(filePath string) ([]byte, error) {
+
+	switch {
+	case strings.HasPrefix(filePath, githubURL):
+		filePath = strings.Replace(filePath, githubURL, "https://raw.githubusercontent.com/", 1)
+		filePath = strings.Replace(filePath, "blob/", "", 1)
+
+	case strings.HasPrefix(filePath, gitlabURL):
+		filePath = strings.Replace(filePath, "-/blob/", "-/raw/", 1)
+
+	}
+
+	body, err := getFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func Load() iter.Seq[PluginManifest] {
+
+	return func(yield func(PluginManifest) bool) {
+		if plugins == "" {
+			return
+		}
+
+		for pluginUrl := range strings.SplitSeq(plugins, ",") {
+			pluginUrl = strings.TrimSpace(pluginUrl)
+			if pluginUrl == "" {
+				logger.Info("GetHandlers", "err", "url is empty")
+				continue
+			}
+
+			manifest := PluginManifest{}
+
+			manifestFile, err := getManifest(pluginUrl)
+			if err != nil {
+				logger.Error("getManifest", "plugin_url", pluginUrl, "err", err)
+				continue
+			}
+
+			if err := yaml.Unmarshal(manifestFile, &manifest); err != nil {
+				logger.Error("plugins.Load yaml Unmarshal failed", "err", err)
+				continue
+			}
+
+			enginesMu.RLock()
+			defer enginesMu.RUnlock()
+
+			if constructor, ok := engines[manifest.Runtime]; ok {
+				handler, err := constructor(manifestFile)
+				if err != nil {
+					logger.Error("constructor failed",
+						"plugin_url", pluginUrl,
+						"plugin_name", manifest.Name,
+						"command", manifest.Command,
+						"err", err)
+					continue
+				}
+
+				manifest.Handler = handler
+
+				if !yield(manifest) {
+					return
+				}
+			}
+
+			logger.Info("plugin loaded", "plugin url", pluginUrl, "plugin name", manifest.Name)
+		}
+	}
+}
---
+++ plugins/wasm/wasm.go
@@ -0,0 +1,95 @@
+package wasm
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	extism "github.com/extism/go-sdk"
+	"github.com/gasoid/merge-bot/handlers"
+	"github.com/gasoid/merge-bot/plugins"
+	"github.com/stretchr/testify/assert/yaml"
+)
+
+type PluginWasmConfig struct {
+	ExportedFunction string   `yaml:"exported_function"`
+	Path             string   `yaml:"path"`
+	EnvVars          []string `yaml:"env_vars"`
+	AllowedHosts     []string `yaml:"allowed_hosts"`
+}
+
+type PluginManifest struct {
+	Name       string           `yaml:"name"`
+	Command    string           `yaml:"command"`
+	WasmConfig PluginWasmConfig `yaml:"wasm_config"`
+}
+
+func init() {
+	plugins.Register("wasm", BuildWasmPlugin)
+}
+
+func BuildWasmPlugin(manifestFile []byte) (plugins.HandlerFunc, error) {
+	manifest := PluginManifest{}
+
+	if err := yaml.Unmarshal(manifestFile, &manifest); err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	envMap := map[string]string{}
+	for _, v := range manifest.WasmConfig.EnvVars {
+		envMap[v] = os.Getenv(strings.ToUpper(v)) // TODO: config.StringVar
+	}
+
+	extismManifest := extism.Manifest{
+		Wasm: []extism.Wasm{
+			extism.WasmFile{
+				Path: manifest.WasmConfig.Path,
+			},
+		},
+		AllowedHosts: manifest.WasmConfig.AllowedHosts,
+		Config:       envMap,
+	}
+
+	config := extism.PluginConfig{
+		EnableWasi: true,
+	}
+
+	compiledPlugin, err := extism.NewCompiledPlugin(ctx, extismManifest, config, []extism.HostFunction{})
+	if err != nil {
+		return nil, err
+	}
+
+	//nolint:errcheck
+	return func(command *handlers.Request, _ string) error {
+		plugin, err := compiledPlugin.Instance(ctx, extism.PluginInstanceConfig{})
+		if err != nil {
+			return fmt.Errorf("can't create instance of plugin %s, error %w", manifest.Name, err)
+		}
+		defer plugin.Close(ctx)
+
+		return command.RunWithContext(func(input []byte) ([]byte, error) {
+			exit, output, err := plugin.Call(manifest.WasmConfig.ExportedFunction, input)
+			if err != nil {
+				return nil, fmt.Errorf("plugin %s returns error: %w", manifest.Name, err)
+			}
+
+			if exit != 0 {
+				return nil, fmt.Errorf("plugin %s returns exit code: %d", manifest.Name, exit)
+			}
+
+			errMessage := plugin.GetError()
+			if errMessage != "" {
+				return nil, fmt.Errorf("plugin %s returns error: %s", manifest.Name, errMessage)
+			}
+
+			if len(output) == 0 {
+				return nil, fmt.Errorf("plugin %s returns nothing", manifest.Name)
+			}
+
+			return output, nil
+		})
+
+	}, nil
+}
