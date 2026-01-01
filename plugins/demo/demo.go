a0f443691d6fb42359a7ea568a7fe5317713fecf
Gasoid <gasoid@gmail.com>
2026-01-01 08:55:21 +0100

    feat: discussion per code line from plugins

--- plugins/demo/demo.go
+++ plugins/demo/demo.go
@@ -6,20 +6,20 @@ import (
 	"github.com/extism/go-pdk"
 )
 
-type PluginInput struct {
+type Input struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Author      string `json:"author"`
 	Diffs       []byte `json:"diffs"`
 }
 
-type PluginOutput struct {
+type Output struct {
 	Comment string `json:"comment"`
 }
 
 //go:wasmexport hello
 func Hello() int32 {
-	input := PluginInput{}
+	input := Input{}
 	if err := pdk.InputJSON(&input); err != nil {
 		pdk.SetError(err)
 		return 1
@@ -31,7 +31,7 @@ func Hello() int32 {
 		return 1
 	}
 
-	output := PluginOutput{
+	output := Output{
 		Comment: "hello " + input.Author + " from " + name,
 	}
 
