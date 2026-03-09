007b81d255a3f49f065515e0ebe8c18067fa78a3
Gasoid <gasoid@gmail.com>
2026-03-09 21:27:47 +0100

    fix: lint

--- handlers/gitlab/pagination.go
+++ handlers/gitlab/pagination.go
@@ -62,11 +62,3 @@ func (g GitlabProvider) listMergeRequestNotes(projectId, mergeId, size int, sort
 		})
 	}, size)
 }
-
-func (g GitlabProvider) listProjectAccessTokens(projectId, size int) iter.Seq[*gitlab.ProjectAccessToken] {
-	return paginate(func(page, perPage int) ([]*gitlab.ProjectAccessToken, *gitlab.Response, error) {
-		return g.client.ProjectAccessTokens.ListProjectAccessTokens(projectId, &gitlab.ListProjectAccessTokensOptions{
-			ListOptions: gitlab.ListOptions{Page: page, PerPage: perPage},
-		})
-	}, size)
-}
